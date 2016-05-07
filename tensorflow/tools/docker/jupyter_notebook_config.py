3f676760ddc58c4c9cdc1fc07331f3a8e03185af
NewGyu <newgyu@kinoboku.net>
2016-05-07 22:44:08 +0900

    python3ベースに変更

--- tensorflow/tools/docker/jupyter_notebook_config.py
+++ tensorflow/tools/docker/jupyter_notebook_config.py
@@ -16,4 +16,4 @@
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
-c.MultiKernelManager.default_kernel_name = 'python2'
+c.MultiKernelManager.default_kernel_name = 'python3'
